dbcb26b31c7aa18371907386408b1137e5f3308c
Mark26-founder <mark26ceo@gmail.com>
2026-08-24 19:45:47 +0500

    Create __init__.py

---
+++ src/aifi/reproduction/__init__.py
@@ -0,0 +1,9 @@
+from aifi.reproduction.models import ReproductionResult, ReproductionStatus
+from aifi.reproduction.engine import reproduce_failure, reproduce_trace
+
+__all__ = [
+    "ReproductionResult",
+    "ReproductionStatus",
+    "reproduce_failure",
+    "reproduce_trace",
+]
