605757c3ce70b9bacd9b594210b71409a3507038
Mark26-founder <mark26ceo@gmail.com>
2026-08-24 19:44:24 +0500

    Create __init__.py

---
+++ src/aifi/diagnosis/__init__.py
@@ -0,0 +1,8 @@
+from aifi.diagnosis.models import DiagnosisFinding, DiagnosisResult
+from aifi.diagnosis.engine import diagnose_failures
+
+__all__ = [
+    "DiagnosisFinding",
+    "DiagnosisResult",
+    "diagnose_failures",
+]
